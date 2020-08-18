97b768483af290ad8f982b16a112741309d1b193
Alexandre DuBreuil <code@alexandredubreuil.com>
2020-08-18 23:26:10 +0200

    Add angular register route

--- client-angular/src/app/app.module.ts
+++ client-angular/src/app/app.module.ts
@@ -5,6 +5,7 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
 import {FormsModule} from "@angular/forms";
 import {HomeComponent} from './home/home.component';
 
@@ -12,6 +13,7 @@ import {HomeComponent} from './home/home.component';
   declarations: [
     AppComponent,
     LoginComponent,
+    RegisterComponent,
     HomeComponent
   ],
   imports: [
---
+++ client-angular/src/app/register/register.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  model: any;
+
+  constructor(private http: HttpClient,
+              private router: Router) {
+  }
+
+  ngOnInit() {
+    this.model = {};
+    sessionStorage.setItem('token', '');
+  }
+
+  register() {
+    this.http
+      .post("http://127.0.0.1:8080/register", {
+        "username": this.model.username,
+        "password": this.model.password,
+      })
+      .subscribe(() => {
+          alert("ok")
+        },
+        error => {
+          alert("ko")
+        })
+  }
+
+}
