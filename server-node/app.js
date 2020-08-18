82e5669e3203729d7d12ae138cff5a848776d662
Alexandre DuBreuil <code@alexandredubreuil.com>
2020-08-18 22:48:45 +0200

    Add sequelize configuration

--- server-node/app.js
+++ server-node/app.js
@@ -38,4 +38,10 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// database
+const db = require("./models");
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and re-sync db.");
+});
+
 module.exports = app;
