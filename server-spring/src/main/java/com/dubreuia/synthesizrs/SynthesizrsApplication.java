60e670b10ba3d14ccbe2aeb9afae53159c9d6843
Alexandre DuBreuil <code@alexandredubreuil.com>
2020-08-18 20:47:29 +0200

    Add spring backend from start.spring.io

---
+++ server-spring/src/main/java/com/dubreuia/synthesizrs/SynthesizrsApplication.java
@@ -0,0 +1,13 @@
+package com.dubreuia.synthesizrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynthesizrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynthesizrsApplication.class, args);
+	}
+
+}
---
+++ server-spring/src/test/java/com/dubreuia/synthesizrs/SynthesizrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dubreuia.synthesizrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynthesizrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
